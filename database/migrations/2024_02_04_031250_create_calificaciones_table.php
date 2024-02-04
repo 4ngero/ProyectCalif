0422b8898d9e2810e193adb603e1628d33c32537
Angelo <crisjmtz@hotmail.com>
2024-02-03 21:30:07 -0600

    Se agregaron las migraciones BD

--- database/migrations/2024_02_04_031250_create_calificaciones_table.php
+++ database/migrations/2024_02_04_031250_create_calificaciones_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('calificaciones', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('id_alumno');
+            $table->unsignedBigInteger('id_materia');
+            $table->foreign('id_alumno')->references('id')->on('alumnos');
+            $table->foreign('id_materia')->references('id')->on('materias');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('calificaciones');
     }
 };
