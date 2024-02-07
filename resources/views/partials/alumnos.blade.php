3b576a3e7d5627cdc398e92f497e192c84bf5cbc
LuisG-Custodio <121039302@upq.edu.mx>
2024-02-07 10:49:14 -0600

    funcionalidades extra

--- resources/views/partials/alumnos.blade.php
+++ resources/views/partials/alumnos.blade.php
@@ -12,19 +12,30 @@
                         <nav class="navbar">
                             <div class="container">
                                 <div class="search-form">
-                                    {{-- buscador --}}
-                                    <form class="d-flex" role="search" action="" method="GET">
-                                        <input class="form-control me-2 col-md-3" type="search" name="_nombre" placeholder="Nombre/Serie/Marca" aria-label="Search">
+                                    <form class="d-flex" role="search" action="/busqueda" method="GET">
+                                        @csrf
+                                        <input class="form-control me-2 col-md-3" type="search" name="_busq" placeholder="Matricula" aria-label="Search">
                                         <button class="btn btn-outline-success" type="submit"> Buscar</button>
                                     </form>
                                 </div>
                             </div>
                         </nav>
                     </div>
-                    <div class="col-4">
+                    <div class="col-4 text-end">
                         <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#Registroalumno">
                             <i class="bi bi-pencil-square"></i> Registrar Alumno
                         </button>                  
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-info" role="alert">
+                            <strong>Leyenda:</strong>
+                            <span class="badge bg-success">Probabilidad de pasar arriba del 70%</span>
+                            <span class="badge bg-info">Probabilidad de pasar entre el 70% y el 30%</span>
+                            <span class="badge bg-danger">Probabilidad de pasar abajo del 30%</span>
+                            <span class="badge bg-warning">Menos de 7 materias inscritas</span>
+                        </div>
                     </div>
                 </div>
                 <table class="table table-striped table-bordered">
@@ -42,7 +53,14 @@
                     </thead>
                     <tbody>
                         @foreach($alumnos as $a)
-                        <tr>
+                        <tr 
+                        @if($a->asignaturas < 7)
+                        @elseif($a->asignaturas >= 7)
+                            @if($a->porcentaje > 70) class="table-success"
+                            @elseif($a->porcentaje <= 70 && $a->porcentaje >= 30) class="table-info"
+                            @else class="table-danger"
+                            @endif
+                    @endif>
                             <td scope="col">{{$a->matricula}}</td>
                             <td scope="col">{{$a->alumnos}}</td>
                             <td scope="col">{{$a->primer_apellido}}</td>
